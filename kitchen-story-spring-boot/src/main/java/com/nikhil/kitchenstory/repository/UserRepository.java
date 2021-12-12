c3bf9b8dcecdd900b67c635325d85dd827df5a70
Nikhil Jain <golu.nikhil4u@gmail.com>
2021-12-12 14:08:49 +0530

    initial commit

---
+++ kitchen-story-spring-boot/src/main/java/com/nikhil/kitchenstory/repository/UserRepository.java
@@ -0,0 +1,15 @@
+package com.nikhil.kitchenstory.repository;
+
+import java.util.Optional;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import com.nikhil.kitchenstory.models.User;
+
+@Repository
+public interface UserRepository extends MongoRepository<User, String> {
+	
+	Optional<User> findByEmail(String email);
+}
+
